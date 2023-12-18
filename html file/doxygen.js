df782cad57a7dd35178e627f907074ae9956622b
chetna199829 <churechetna@gmail.com>
2023-12-18 15:54:02 +0530

    html css code till date

---
+++ html file/doxygen.js
@@ -0,0 +1,10 @@
+/**
+ * this function will give some output
+ * @param {*} param 
+ */
+function show( param){
+    console.log("sfsgdjffffffe")
+}
+//doxygen hamne extention se donwload kiya
+ //jise hame param is parameter ke liye comment 
+ // apne ap comment milti he 
---
+++ html file/sample.js
@@ -0,0 +1,7 @@
+//single line
+
+/** 
+ sjfek;WebGLTransformFeedback;fsk;fddk;fk;adajflsdfl
+ sjfllsjdflflsjfld.vmcjflsdkfs;fks;
+ sfdk;getComputedStyle;sdk;k
+ */
